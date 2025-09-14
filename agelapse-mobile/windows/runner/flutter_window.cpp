50dd460437cff195aff9ea8eeec36d3d5055adf8
hugocornellier <hugo.cornellier@gmail.com>
2025-09-14 14:35:40 -0300

    force dark theme on mac: agelapse is currently dark theme only and if the user has light theme on their pc, it looks off.

--- agelapse-mobile/windows/runner/flutter_window.cpp
+++ agelapse-mobile/windows/runner/flutter_window.cpp
@@ -1,6 +1,8 @@
 #include "flutter_window.h"
 
 #include <optional>
+#include <dwmapi.h>
+#pragma comment(lib, "Dwmapi.lib")
 
 #include "flutter/generated_plugin_registrant.h"
 
@@ -16,11 +18,8 @@ bool FlutterWindow::OnCreate() {
 
   RECT frame = GetClientArea();
 
-  // The size here must match the window dimensions to avoid unnecessary surface
-  // creation / destruction in the startup path.
   flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
       frame.right - frame.left, frame.bottom - frame.top, project_);
-  // Ensure that basic setup of the controller was successful.
   if (!flutter_controller_->engine() || !flutter_controller_->view()) {
     return false;
   }
@@ -31,10 +30,12 @@ bool FlutterWindow::OnCreate() {
     this->Show();
   });
 
-  // Flutter can complete the first frame before the "show window" callback is
-  // registered. The following call ensures a frame is pending to ensure the
-  // window is shown. It is a no-op if the first frame hasn't completed yet.
   flutter_controller_->ForceRedraw();
+
+  HWND hwnd = GetHandle();
+  BOOL useDark = TRUE;
+  DwmSetWindowAttribute(hwnd, static_cast<DWMWINDOWATTRIBUTE>(20), &useDark, sizeof(useDark));
+  DwmSetWindowAttribute(hwnd, static_cast<DWMWINDOWATTRIBUTE>(19), &useDark, sizeof(useDark));
 
   return true;
 }
@@ -51,7 +52,6 @@ LRESULT
 FlutterWindow::MessageHandler(HWND hwnd, UINT const message,
                               WPARAM const wparam,
                               LPARAM const lparam) noexcept {
-  // Give Flutter, including plugins, an opportunity to handle window messages.
   if (flutter_controller_) {
     std::optional<LRESULT> result =
         flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam,
